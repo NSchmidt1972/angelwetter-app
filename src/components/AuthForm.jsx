c861a2be32f03249d5925012c106e971c24ac660
NSchmidt1972 <blackberry@schmidt-2006.de>
2025-06-07 16:14:33 +0000

    Vor- und Nachname als Placeholder ergänzt

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -139,7 +139,7 @@ export default function AuthForm() {
           type="text"
           name="name"
           autoComplete="name"
-          placeholder="Name"
+          placeholder="Vor und Nachname"
           value={name}
           onChange={(e) => setName(e.target.value)}
           className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400  dark:bg-gray-800 dark:text-white dark:placeholder-blue-400"
